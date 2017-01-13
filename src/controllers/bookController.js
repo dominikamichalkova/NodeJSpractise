bda5db64b12509919409dc737ba9d3bfc9573bdb
Dominika <dmichalk@sk.ibm.com>
2017-01-13 12:35:58 +0100

    changes in routes and controllers

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,3 +1,55 @@
-var bookController = function () {};
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+
+var bookController = function (bookService, nav) {
+    var getIndex = function (req, res) {
+        var url = 'mongodb://localhost:27017/books'; //auto create a new db instance or use an existing one
+        mongodb.connect(url, function (err, db) {
+            var collection = db.collection('booklist'); //pull out of our db
+            collection.find({}).toArray(function (err, results) {
+                res.render('bookListView', { //reference to bookListView.ejs, look through in views folder app.set is pointing
+                    title: 'Books', //json object
+                    nav: nav,
+                    books: results //books:books when hardcode defined books in code, passing in an array that is navigation and my; now we are taking books from database to be printed out on site
+                });
+            });
+        });
+
+
+    };
+
+
+    var getById = function (req, res) {
+
+        var id = new objectId(req.params.id);
+        var url = 'mongodb://localhost:27017/books';
+
+        mongodb.connect(url, function (err, db) {
+            var collection = db.collection('booklist'); //pull out of our db
+            collection.findOne({ //findOne return just the one ID, must createobjectId = _id mongoDB uses for ID
+                    _id: id
+                },
+                function (err, results) {
+                    res.render('bookView', {
+                        title: 'Books',
+                        nav: nav,
+                        book: results
+                    });
+                });
+        });
+        //var id = req.params.id;
+        //everything that is behind : Express is going to attach to req object
+        //res.send('Hello single books');
+        //res.render('bookView', {
+        //title: 'Books', //json object
+        //nav: nav,
+        //book: books[id] //just one book to render associated with the ID
+    };
+
+    return {
+        getIndex: getIndex,
+        getById: getById //controller sserves 2 routes, convenient way of having a controller when we have even more routes
+    };
+};
 
 module.exports = bookController;
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,59 +1,19 @@
 var express = require('express');
-
 var bookRouter = express.Router(); //function
 var mongodb = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
 
 //wrapper var router that wrapps the routes
 var router = function (nav) {
-
+    var bookController = require('../controllers/bookController')(null, nav);
     bookRouter.route('/') // /Books/ 
-        .get(function (req, res) {
-            var url = 'mongodb://localhost:27017/books'; //auto create a new db instance or use an existing one
-            mongodb.connect(url, function (err, db) {
-                var collection = db.collection('booklist'); //pull out of our db
-                collection.find({}).toArray(function (err, results) {
-                    res.render('bookListView', { //reference to bookListView.ejs, look through in views folder app.set is pointing
-                        title: 'Books', //json object
-                        nav: nav,
-                        books: results //books:books when hardcode defined books in code, passing in an array that is navigation and my; now we are taking books from database to be printed out on site
-                    });
-                });
-            });
-
-
-        });
+        .get(bookController.getIndex); //now we return controllers
     //.post() for creating users 
 
     bookRouter.route('/:id') // read more about a single book, pulling in our id
-        .get(function (req, res) {
-
-            var id = new objectId(req.params.id);
-            var url = 'mongodb://localhost:27017/books';
-
-            mongodb.connect(url, function (err, db) {
-                var collection = db.collection('booklist'); //pull out of our db
-                collection.findOne({ //findOne return just the one ID, must createobjectId = _id mongoDB uses for ID
-                        _id: id
-                    },
-                    function (err, results) {
-                        res.render('bookView', {
-                            title: 'Books',
-                            nav: nav,
-                            book: results
-                        });
-                    });
-            });
-            //var id = req.params.id;
-            //everything that is behind : Express is going to attach to req object
-            //res.send('Hello single books');
-            //res.render('bookView', {
-            //title: 'Books', //json object
-            //nav: nav,
-            //book: books[id] //just one book to render associated with the ID
-        });
+        .get(bookController.getById);
     return bookRouter;
 };
-//we have 2 routes hosted by this router
+//we have 2 routes hosted by this router, 
 
 module.exports = router; //accessing it by export function that creates a router
